0e91914f80e9a0e66754b923c9797547f8e53c9d
Guilherme <jg21504956@gmail.com>
2026-03-02 15:32:49 -0300

    advantages and disadvantages of SOLID

---
+++ modulo7/index.ts
@@ -0,0 +1,18 @@
+/*
+    Vantagens E Desvantagens do SOLID
+    
+    Vantagens:
+    - Código modular
+    - Código reutilizável (DRY)
+    - Código testável , baixo acoplamento
+    - Baixo acoplamento e alta coesão
+    - Facíl de expansão
+    - Separação de conceitos
+    - Facíl manutenção
+
+    Desvantagens:
+    - Complexidade 
+    - Quantidade maior de código
+    - Tenha cuidado com YAGNI , KISS 
+    - Aumento no tempo de desenvolvimento
+*/
